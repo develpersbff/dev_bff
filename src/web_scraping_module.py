e746fdffb58ef273020863cdfd441b48afa8b5ff
Erik M. Dodd <emdodd27@gmail.com>
2023-01-17 15:26:48 -0800

    Able to grab the top search result from DuckDuckGo

--- src/web_scraping_module.py
+++ src/web_scraping_module.py
@@ -1,35 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
+import re
+
+# def tag_selector():
+#     return tag.name == "a" and tag.has_attr("data-testid") and "result-title-a" in tag.get("data-testid")
+
 
 
 def web_scraper(query):
-    user_input = str(input('>>'))
-    req = requests.get(f'https://duckduckgo.com/?q=' + user_input)
-    soup = BeautifulSoup(req.text, 'html.parser')
-    top_result_class = soup.findAll('div', data_testid='result-title-a')
-    # top_results_data = soup.find(data_testid='result-title-a')
+    with sync_playwright() as p:
+        user_input = str(input('>>'))
+        browser = p.chromium.launch(headless=False, slow_mo=1000)
+        # browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(f'https://duckduckgo.com/?q=' + user_input)
+        # top_result = page.get_by_text('r1-0')
+        # print(top_result)
+        content = page.content()
+        # beautiful soup and run parsing function
+        soup = BeautifulSoup(content, features='html.parser')
+        top_search = soup.find(id='r1-0')
+        # A special attribution to ChatGPT for giving us the regex pattern below:
+        pattern = r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
+
+        matches = re.findall(pattern, str(top_search))
+        top_result_link = matches[0][:-1]
+        print(top_result_link)
+
+        # print(top_link)
 
 
 
-    # for data in top_result_div:
-    #     if data.find(class_='g'):
-    #         print(data)
+        # browser.close()
 
 
-
-    # soup = BeautifulSoup(top_result_div, 'xml')
-    # top_result_class = soup.findAll(class_='g')
-    # top_result_class = soup.find(class_='g')
-    # title = top_result.find('h3').text
-    # url = top_result.find('a')['href']
-    # text = top_result.get_text()
-    # scrape_results = {'title': title, 'url': url, 'text': text}
-    # print(scrape_results)
-    # return scrape_results
-    print('top_result', top_result_class)
-
-    # print('req.text', req.text)
-    # print(len(top_result_div))
 
 web_scraper('query')
 
