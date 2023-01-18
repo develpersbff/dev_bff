8238e7b190136c8638e8569553290479b08d97f8
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-01-17 18:18:21 -0800

    some progress on the article scraping function

--- src/web_scraping_module.py
+++ src/web_scraping_module.py
@@ -11,20 +11,22 @@ import re
 def web_scraper(query):
     with sync_playwright() as p:
         user_input = str(input('>>'))
-        browser = p.chromium.launch(headless=False, slow_mo=1000)
+        browser = p.chromium.launch(slow_mo=1000)
+        # browser = p.chromium.launch(headless=False, slow_mo=1000
         # browser = p.chromium.launch()
         page = browser.new_page()
         page.goto(f'https://duckduckgo.com/?q=' + user_input)
-        # top_result = page.get_by_text('r1-0')
-        # print(top_result)
         content = page.content()
         # beautiful soup and run parsing function
         soup = BeautifulSoup(content, features='html.parser')
         top_search = soup.find(id='r1-0')
+        print(f"this is the r1-0 {top_search}")
         # A special attribution to ChatGPT for giving us the regex pattern below:
-        pattern = r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
+        pattern = r"(^https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
+
 
         matches = re.findall(pattern, str(top_search))
+        print(f"this is the matches {matches}")
         top_result = matches[0][:-1]
         top_result_link = "https://" + top_result
         print(top_result_link)
@@ -35,17 +37,21 @@ def article_scraper(web_scraper):
     top_result_link = web_scraper('query')
     print(f'this is the top result link {top_result_link}')
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=1000)
+        browser = p.chromium.launch(slow_mo=1000)
+        # browser = p.chromium.launch(headless=False, slow_mo=1000)
         # browser = p.chromium.launch()
         page = browser.new_page()
         page.goto(top_result_link)
-        # top_result = page.get_by_text('r1-0')
-        # print(top_result)
         content = page.content()
         # beautiful soup and run parsing function
         soup = BeautifulSoup(content, features='html.parser')
         # raw_text = page.inner_html("#article").text
-        print(soup)
+        p_tags = soup.find_all('p')
+        for p in p_tags:
+            print(p.get_text())
+        # soup_main = soup.find('main')
+        # p_tags = soup_main.find_all('p')
+        # print(p_text)
 
         # print(top_link)
 
