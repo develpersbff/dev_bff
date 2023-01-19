5c2b21a544d91bd18a3c8cf4d06ee22247a1a4f6
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-01-18 20:00:06 -0800

    added whats next function to allow additional inputs and options after main search

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from api_module import api_call
-from web_scraping_module import web_scraper
+from web_scraping_module import web_scraper, web_scraper_second
+import sys
 
 
 article_sample = "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus."
@@ -12,12 +13,36 @@ def welcome():
     Your best friend in learning! 
     Input a search term to 
     retrieve summarized articles.
+    *Be patient, results take a few seconds to load*
     Have fun!
     *********************************
     """)
+
+
+def what_next():
+    print("""
+    What would you like to do next? 
+    "n" for a different article, 
+    "l" to get a link to the article, 
+    "q" to quit,
+    """)
+    user_input2 = str(input('>> '))
+    if user_input2.lower() == "n":
+        api_call(web_scraper_second(user_input))
+        print("thank you for using dev_BFF, see you next time!")
+        return
+    if user_input2.lower() == "l":
+        url = web_scraper(user_input)
+        print(f"{url}\n")
+        print("thank you for using dev_BFF, see you next time!")
+        return
+    if user_input2.lower() == "q":
+        print("thank you for using dev_BFF, see you next time!")
+        return
 
 
 if __name__ == '__main__':
     welcome()
     user_input = str(input('>> '))
     api_call(web_scraper(user_input))
+    what_next()
